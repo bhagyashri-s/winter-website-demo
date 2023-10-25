af058dd358add4cc2339453ed233769e256de7df
bhagyashrishinde1996 <bhagyashritupe96@gmail.com>
2023-10-25 21:21:51 +0530

    Updated bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # Setup backend using AWS S3 (Remote State File) and DynamoDB (lock)
 terraform {
   backend "s3" {
-    bucket         = "ctl-tf-state-lock-demo"
+    bucket         = "ctl-tf-state-lock-demo-1"
     key            = "s3"
     region         = "ap-south-1"
     dynamodb_table = "tf-lock-state"
